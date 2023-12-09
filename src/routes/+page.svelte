c56a97b0e19c18e28416e28938ac756c8e7cb8e7
Jonathan Bangert <jonathan@bangert.dk>
2023-12-09 09:51:41 +0100

    Add initial project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			Welcome to Academia
+		</span>
+	</h1>
+
+	<h2>
+		The only asignment editor you'll ever need
+	</h2>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / (2048 * 4)) 0;
+	}
+</style>
