89b69d5c9850e7b230dfddc4fb0b99cfa8e86d96
Jonathan Bangert <jonathan@bangert.dk>
2023-12-12 18:54:05 +0100

    Add collaboration cursor

--- src/routes/workspace/Tiptap.svelte
+++ src/routes/workspace/Tiptap.svelte
@@ -3,6 +3,7 @@
     import { Editor } from '@tiptap/core'
     import StarterKit from '@tiptap/starter-kit'
     import Collaboration from '@tiptap/extension-collaboration'
+    import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
     import * as Y from 'yjs'
     import { HocuspocusProvider } from '@hocuspocus/provider'
   
@@ -30,6 +31,13 @@
           editor = new Editor({
             element: element,
             extensions: [
+              CollaborationCursor.configure({
+                provider,
+                user: {
+                  name: 'Test user',
+                  color: '#f783ac',
+                },
+              }),
               StarterKit.configure({
                 history: false,
               }),
@@ -79,13 +87,40 @@
   
   <div id="editor" bind:this={element} />
   
-  <style>
-    button.active {
-      background: rgb(0, 0, 0);
-      color: white;
-    }
+<style>
+  button.active {
+    background: rgb(0, 0, 0);
+    color: white;
+  }
 
-    :global(.ProseMirror:focus) {
-        outline: none;
-    }
-  </style>
+  :global(.ProseMirror:focus) {
+      outline: none;
+  }
+
+  /* Give a remote user a caret */
+  :global(.collaboration-cursor__caret) {
+    border-left: 1px solid #0d0d0d;
+    border-right: 1px solid #0d0d0d;
+    margin-left: -1px;
+    margin-right: -1px;
+    pointer-events: none;
+    position: relative;
+    word-break: normal;
+  }
+
+  /* Render the username above the caret */
+  :global(.collaboration-cursor__label) {
+    border-radius: 3px 3px 3px 0;
+    color: #0d0d0d;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 600;
+    left: -1px;
+    line-height: normal;
+    padding: 0.1rem 0.3rem;
+    position: absolute;
+    top: -1.4em;
+    user-select: none;
+    white-space: nowrap;
+  }
+</style>
