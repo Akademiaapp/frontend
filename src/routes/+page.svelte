dc452b4c45848d2f85a28d385d7c8fb972b772e4
Jonathan Bangert <jonathan@bangert.dk>
2023-12-15 17:36:53 +0100

    Fix typings and stuff with auth state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,13 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
 	import { goto } from '$app/navigation';
+	import type { AuthorizerState } from '@authorizerdev/authorizer-svelte/types';
+	import type { Readable } from 'svelte/store';
 
-	interface AuthorizerState {
-		token: string;
-		user: any;
-		loading: boolean;
-		logout: Function;
-		subscribe: Function;
-	};
 
 	let state: AuthorizerState;
 
-	const store: AuthorizerState = getContext('authorizerContext');
+	const store = <Readable<AuthorizerState>>getContext('authorizerContext');
 
 	store.subscribe((data: AuthorizerState) => {
 		state = data;
