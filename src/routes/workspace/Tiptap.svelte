2af77171db2305d5c264148c68bd4299d6dad1cc
Jonathan Bangert <jonathan@bangert.dk>
2024-01-12 23:10:10 +0100

    Maybe fix??

--- src/routes/workspace/Tiptap.svelte
+++ src/routes/workspace/Tiptap.svelte
@@ -68,7 +68,9 @@
 								color: '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0')
 							}
 						}),
-						Collaboration,
+						Collaboration.configure({
+							document: provider.document,
+						}),
 						Document.extend({
 							content: 'title block+'
 						}),
