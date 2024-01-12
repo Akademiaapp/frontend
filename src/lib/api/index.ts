dd69572bc6553d2ede875c82aa4112d98fe91d40
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-01-12 22:27:25 +0100

    fix debounce

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,67 +1,87 @@
-import type { AuthorizerState } from "@authorizerdev/authorizer-svelte/types";
+import type { AuthorizerState } from '@authorizerdev/authorizer-svelte/types';
 import { get, type Readable } from 'svelte/store';
 
 export default class ApiHandler {
-  static baseUrl = 'https://api.akademia.cc';
-  static context: Readable<AuthorizerState>;
-  constructor(context: Readable<AuthorizerState>) {
-    ApiHandler.context = context
-  }
+	static baseUrl = 'https://api.akademia.cc';
+	static context: Readable<AuthorizerState>;
+	constructor(context: Readable<AuthorizerState>) {
+		ApiHandler.context = context;
+	}
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  debounce(func: any, timeout = 300){
-    let timer: number;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return (...args: any) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => { func.apply(this, args); }, timeout);
-    };
-  }
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	debounce(func: any, timeout = 300) {
+		let timer: number;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		return (...args: any) => {
+			clearTimeout(timer);
+			timer = setTimeout(() => {
+				func.apply(this, args);
+			}, timeout);
+		};
+	}
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  callApi(endpoint: string, options?: any, method: string = 'GET') {
-    const url = ApiHandler.baseUrl + endpoint;
-    // Add bearer token to headers
-    const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${get(ApiHandler.context)?.token?.access_token || ''}`,
-    };
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	callApi(endpoint: string, options?: any, method: string = 'GET') {
+		const url = ApiHandler.baseUrl + endpoint;
+		// Add bearer token to headers
+		const headers = {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${get(ApiHandler.context)?.token?.access_token || ''}`
+		};
 
-    return fetch(url + '?' + new URLSearchParams({
-      ...options
-    }), {
-      method,
-      headers
-    })
-  }
+		return fetch(
+			url +
+				'?' +
+				new URLSearchParams({
+					...options
+				}),
+			{
+				method,
+				headers
+			}
+		);
+	}
 
-  getDocument(documentId: string) {
-    return this.callApi('/documents/' + documentId)
-  }
+	getDocument(documentId: string) {
+		return this.callApi('/documents/' + documentId);
+	}
 
-  getUserDocuments() {
-    return this.callApi('/documents')
-  }
+	getUserDocuments() {
+		return this.callApi('/documents');
+	}
 
-  createDocument(documentName: string) {
-    return this.callApi('/documents', {
-      name: documentName,
-      user_id: get(ApiHandler.context).user?.id,
-    }, 'POST')
-  }
+	createDocument(documentName: string) {
+		return this.callApi(
+			'/documents',
+			{
+				name: documentName,
+				user_id: get(ApiHandler.context).user?.id
+			},
+			'POST'
+		);
+	}
 
-  addUserToDocument(documentId: string, user_email: string) {
-    return this.callApi('/documents/' + documentId + '/users', {
-      user_email
-    }, 'PUT')
-  }
+	addUserToDocument(documentId: string, user_email: string) {
+		return this.callApi(
+			'/documents/' + documentId + '/users',
+			{
+				user_email
+			},
+			'PUT'
+		);
+	}
 
-  renameDocument(documentId: string, documentName: string) {
-    return this.debounce(
-      this.callApi('/documents/' + documentId, {
-        name: documentName
-        }, 'PUT'
-      ), 1000
-    )
-  }
+	renameDocument(documentId: string, documentName: string) {
+		return this.debounce(
+			() =>
+				this.callApi(
+					'/documents/' + documentId,
+					{
+						name: documentName
+					},
+					'PUT'
+				),
+			1000
+		);
+	}
 }
