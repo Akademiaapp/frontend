b787e0ae9323db7c5f33ef21bcfdf7c9f73d7cb8
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-01-14 19:12:50 +0100

    refactor workspace

--- src/routes/workspace/+page.svelte
+++ src/routes/workspace/+page.svelte
@@ -1,84 +1,18 @@
 <script lang="ts">
-	import { getContext } from 'svelte';
-	import { goto } from '$app/navigation';
 	import Toolbar from './Toolbar.svelte';
-	import Tiptap from './Tiptap.svelte';
-	import Sidebar from './Sidebar.svelte';
-	import type { Editor } from '@tiptap/core';
-	import type { Readable } from 'svelte/store';
-	import type { AuthorizerState } from '@authorizerdev/authorizer-svelte/types';
 	import FileEditor from './FileEditor.svelte';
-
-	let state: AuthorizerState;
-
-	const store = <Readable<AuthorizerState>>getContext('authorizerContext');
-
-	store.subscribe((data: AuthorizerState) => {
-		state = data;
-		if (!state.user?.preferred_username) {
-			goto('/signin');
-		}
-	});
+	import Workspace from './Workspace.svelte';
+	import type { Editor } from '@tiptap/core';
 
 	let editor: Editor;
 
 	let activeFile: string;
 	let activeFilename: string;
-
-	let sidebarVisible: boolean = true;
-
-	$: console.log(sidebarVisible);
 </script>
 
-<div class="container">
-	<div class="sidebar" class:hidden={!sidebarVisible}>
-		<Sidebar bind:activeFile bind:sidebarVisible bind:activeFilename></Sidebar>
-	</div>
-
+<Workspace bind:activeFile bind:activeFilename>
 	<div class="editor">
 		<Toolbar {editor} {activeFile} />
 		<FileEditor bind:editor {activeFile} {activeFilename}></FileEditor>
 	</div>
-</div>
-
-<style lang="scss">
-	.container {
-		display: flex;
-		flex-direction: row;
-	}
-
-	.sidebar {
-		height: 100%;
-		z-index: 1;
-		float: left;
-		position: sticky;
-		top: 0;
-		gap: 0.75rem;
-		display: flex;
-		pointer-events: auto;
-
-		width: 250px;
-		padding: var(--pad);
-		height: 100%;
-		height: calc(100vh - var(--pad) - var(--pad));
-
-		transition: all 200ms cubic-bezier(0, 0, 0.24, 1.11);
-
-		&.hidden {
-			margin-left: calc(-250px - var(--pad));
-		}
-	}
-
-	.editor {
-		flex-grow: 1;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		width: calc(100% - 250px);
-		gap: 0.75rem;
-		margin-top: var(--pad);
-
-		transition: all 300ms;
-	}
-</style>
+</Workspace>
