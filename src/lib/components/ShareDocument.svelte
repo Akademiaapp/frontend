647562d4dd433bb52d6ba2674366502cb2dbcd7f
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-01-22 22:39:39 +0100

    trying to add a searchbar to  people

--- src/lib/components/ShareDocument.svelte
+++ src/lib/components/ShareDocument.svelte
@@ -1,21 +1,39 @@
 <script lang="ts">
-  import * as Dialog from "$lib/components/ui/dialog";
-	import { UserRoundPlus } from "lucide-svelte";
-  import { buttonVariants } from "$lib/components/ui/button";
+	import * as Dialog from '$lib/components/ui/dialog';
+	import { UserRoundPlus } from 'lucide-svelte';
+	import { buttonVariants } from '$lib/components/ui/button';
+	import * as Command from '$lib/components/ui/command';
+
+	let open: boolean;
 </script>
 
-<Dialog.Root>
-  <Dialog.Trigger class={"br-2 absolute flex right-0 h-full toolbar-bar py-2 px-4"+buttonVariants({ variant: "floating" })}>
-    <UserRoundPlus size={17} />
-    <span class="font-semibold text leading-normal overflow-hidden box-border">Inviter</span>
-  </Dialog.Trigger>
-  <Dialog.Content>
-    <Dialog.Header>
-      <Dialog.Title>Are you sure absolutely sure?</Dialog.Title>
-      <Dialog.Description>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </Dialog.Description>
-    </Dialog.Header>
-  </Dialog.Content>
+<Dialog.Root bind:open>
+	<Dialog.Trigger
+		class={'br-2 toolbar-bar absolute right-0 flex h-full px-4 py-2' +
+			buttonVariants({ variant: 'floating' })}
+	>
+		<UserRoundPlus size={17} />
+		<span class="text box-border overflow-hidden font-semibold leading-normal">Inviter</span>
+	</Dialog.Trigger>
 </Dialog.Root>
+<Command.Dialog bind:open>
+	<Command.Input placeholder="Type a command or search..." />
+	<Command.List>
+		<Command.Empty>No results found.</Command.Empty>
+		<Command.Group heading="Suggestions">
+			<Command.Item>Calendar</Command.Item>
+			<Command.Item>Search Emoji</Command.Item>
+			<Command.Item>Caclculator</Command.Item>
+			<Command.Item>Calcuslatbor</Command.Item>
+			<Command.Item>Calcxsulator</Command.Item>
+			<Command.Item>Calxcuslator</Command.Item>
+			<Command.Item>Calcuxxlsator</Command.Item>
+		</Command.Group>
+		<Command.Separator />
+		<Command.Group heading="Settings">
+			<Command.Item>Profile</Command.Item>
+			<Command.Item>Billing</Command.Item>
+			<Command.Item>Settings</Command.Item>
+		</Command.Group>
+	</Command.List>
+</Command.Dialog>
