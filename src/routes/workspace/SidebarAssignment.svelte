916033c472f7bdd7d44a6f7e470bf3f5ab7219b0
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-03-12 15:54:34 +0100

    refactor workspace using +layout, and fix SidebarAssignment problem

--- src/routes/workspace/SidebarAssignment.svelte
+++ src/routes/workspace/SidebarAssignment.svelte
@@ -19,7 +19,7 @@
 
 	store.subscribe((data: AuthorizerState) => {
 		state = data;
-		console.log("hahaahhaaggag 	", state);
+		console.log('hahaahhaaggag 	', state);
 	});
 
 	export let assignmentId: string = '26e29956-af96-4b22-b088-2a0d5d1474ad';
@@ -28,11 +28,11 @@
 			...EditorExtensions,
 			Title,
 			Document.extend({
-				content: 'title block+',
-			}),
+				content: 'title block+'
+			})
 		],
 		content: api.getDocumentJson(assignmentId),
-		editable: false,
+		editable: false
 	});
 </script>
 
@@ -56,9 +56,38 @@
 		<div class="assignment-tiptap">
 			<h1>Summum jus, summa injura - Analyse</h1>
 			<p>I denne time vil vi analysere billedet "Summum jus, summa injura"</p>
-			<p><strong><em>Erik Henningsens 'Summum jus, summa injuria. Barnemordet' fra<span class="collaboration-cursor__caret ProseMirror-widget" style="border-color: #678c6b" contenteditable="false"><div class="collaboration-cursor__label" style="background-color: #678c6b">dbtracker.dk@gmail.com</div></span> 1889</em></strong></p>
-			<img src="https://www.hirschsprung.dk/storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBaGtCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--c5ad5e056c3779faa020f14beb037772e00fc75a/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBTU0lJYW5CbkJqb0dSVlE2RTNKbGMybDZaVjkwYjE5bWFXeHNXd2RwQXRBSGFRSXpCQT09IiwiZXhwIjpudWxsLCJwdXIiOiJ2YXJpYXRpb24ifX0=--918b982eee23d4bacc9db46fa526b6f6b97abd53/Henningsen-E-154-HBD.jpg" contenteditable="false" draggable="true" class="ProseMirror-selectednode">
-			<cl><li><p> Hvad mon titlen betyder? Spørg Google.&nbsp;</p><p>Hvad mener du om titlen?</p></li><li><p>Kig på maleriet igen, hvad tror du, der er sket? Brug din viden om tiden fra dine noter.</p></li><li><p><br class="ProseMirror-trailingBreak"></p></li></ol>
+			<p>
+				<strong
+					><em
+						>Erik Henningsens 'Summum jus, summa injuria. Barnemordet' fra<span
+							class="collaboration-cursor__caret ProseMirror-widget"
+							style="border-color: #678c6b"
+							contenteditable="false"
+							><div class="collaboration-cursor__label" style="background-color: #678c6b">
+								dbtracker.dk@gmail.com
+							</div></span
+						> 1889</em
+					></strong
+				>
+			</p>
+			<img
+				src="https://www.hirschsprung.dk/storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBaGtCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--c5ad5e056c3779faa020f14beb037772e00fc75a/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBTU0lJYW5CbkJqb0dSVlE2RTNKbGMybDZaVjkwYjE5bWFXeHNXd2RwQXRBSGFRSXpCQT09IiwiZXhwIjpudWxsLCJwdXIiOiJ2YXJpYXRpb24ifX0=--918b982eee23d4bacc9db46fa526b6f6b97abd53/Henningsen-E-154-HBD.jpg"
+				contenteditable="false"
+				draggable="true"
+				class="ProseMirror-selectednode"
+			/>
+			<ol>
+				<li>
+					<p>Hvad mon titlen betyder? Spørg Google.&nbsp;</p>
+					<p>Hvad mener du om titlen?</p>
+				</li>
+				<li>
+					<p>
+						Kig på maleriet igen, hvad tror du, der er sket? Brug din viden om tiden fra dine noter.
+					</p>
+				</li>
+				<li><p><br class="ProseMirror-trailingBreak" /></p></li>
+			</ol>
 		</div>
 	</div>
 </nav>
