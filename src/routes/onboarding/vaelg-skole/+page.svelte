da6b114455c17f2101dfa1f5485f108a986bcd22
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-03-13 23:00:19 +0100

    too late for good commit messages

--- src/routes/onboarding/vaelg-skole/+page.svelte
+++ src/routes/onboarding/vaelg-skole/+page.svelte
@@ -19,15 +19,9 @@
 	<CommandList>
 		<CommandEmpty>No results found.</CommandEmpty>
 		<CommandGroup heading="Suggestions">
-			<CommandItem>Calendar</CommandItem>
+			<CommandItem>Sct. Jørgens skole - Næstved</CommandItem>
 			<CommandItem>Search Emoji</CommandItem>
 			<CommandItem>Calculator</CommandItem>
-		</CommandGroup>
-		<CommandSeparator />
-		<CommandGroup heading="Settings">
-			<CommandItem>Profile</CommandItem>
-			<CommandItem>Billing</CommandItem>
-			<CommandItem>Settings</CommandItem>
 		</CommandGroup>
 	</CommandList>
 </Command>
