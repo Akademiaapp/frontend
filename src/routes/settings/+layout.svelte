bdb3cea22d215eea04ab963f3e26b41d421029cf
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-03-20 12:59:06 +0100

    add settings layout

---
+++ src/routes/settings/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<div class="p-6">
+	<h1>Settings</h1>
+
+	<p>Settings page</p>
+
+	<div class="flex">
+		<div class="flex flex-col gap-4">
+			<a href="appearance">Appearance</a>
+			<a href="account">Account</a>
+			<a href="account">Aula Integration</a>
+		</div>
+	</div>
+</div>
