4723c20dea6bc91bb2e5913ac16ae390133d1312
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-03-20 17:47:01 +0100

    Add order

--- src/routes/workspace/home/guide/+page.svelte
+++ src/routes/workspace/home/guide/+page.svelte
@@ -7,27 +7,32 @@
 	const steps = [
 		{
 			title: 'Velkommen til Akademia!',
-			content: 'Dette er en guide for at hjælpe dig i gang.'
+			content: 'Dette er en guide for at hjælpe dig i gang.',
+			order: 0
 		},
 		{
 			target: '.sidebar',
 			title: 'Sidebaren',
-			content: 'Sidebaren giver dig et hurtigt overblik over alle dine filer og funktioner.'
+			content: 'Sidebaren giver dig et hurtigt overblik over alle dine filer og funktioner.',
+			order: 1
 		},
 		{
 			target: '#quick-bar',
 			title: 'Quick Actions',
-			content: 'Her har du adgang til de vigtiste funktioner, som dashboarded, søge og timeren.'
+			content: 'Her har du adgang til de vigtiste funktioner, som dashboarded, søge og timeren.',
+			order: 2
 		},
 		{
 			target: '#overview',
 			title: 'Oversigt',
-			content: 'Her kan du se dine Afleveringe og aktive filer'
+			content: 'Her kan du se dine Afleveringe og aktive filer',
+			order: 3
 		},
 		{
 			target: '#calendar',
 			title: 'Kalender',
-			content: 'Her kan du se dine lektioner og andre begivenheder.'
+			content: 'Her kan du se dine lektioner og andre begivenheder.',
+			order: 4
 		}
 	];
 	tg.addSteps(steps);
