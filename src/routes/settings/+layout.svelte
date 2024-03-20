51fb657f7096b6452adafd0ef1f52a898952dca5
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-03-20 16:39:13 +0100

    Update +layout.svelte

--- src/routes/settings/+layout.svelte
+++ src/routes/settings/+layout.svelte
@@ -1,4 +1,21 @@
 <script>
+	import { buttonVariants } from '@/components/ui/button';
+	import { cn } from '@/utils';
+
+	settingPages = [
+		{
+			name: 'Appearance',
+			path: 'appearance'
+		},
+		{
+			name: 'Account',
+			path: 'account'
+		},
+		{
+			name: 'Aula Integration',
+			path: 'aula'
+		}
+	];
 </script>
 
 <div class="p-6">
@@ -8,9 +25,9 @@
 
 	<div class="flex">
 		<div class="flex flex-col gap-4">
-			<a href="appearance">Appearance</a>
-			<a href="account">Account</a>
-			<a href="account">Aula Integration</a>
+			{#each settingPages as page}
+				<a class={cn(buttonVariants({ variant: 'ghost' }))} href={page.path}>{page.name}</a>
+			{/each}
 		</div>
 	</div>
 </div>
