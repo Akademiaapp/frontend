8f02e3144ec58ae7f4ddd5373bf55936a1602d44
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-03-22 19:55:18 +0100

    Update +page.svelte

---
+++ src/routes/settings/aula/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Input } from '$lib/components/ui/input';
+
+	let username = '';
+	let password = '';
+
+	$: console.log(username, password);
+</script>
+
+<form>
+	<Input type="text" placeholder="Username" bind:value={username} />
+	<Input type="password" placeholder="Password" bind:value={password} />
+</form>
