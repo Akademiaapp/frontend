6d49b703f1b07d0ff450161e5ba58117b8f13055
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-03-23 16:55:31 +0100

    Fix signup screen

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Button from '@/components/ui/button/button.svelte';
-	import UserAuthForm from './UserAuthForm.svelte';
+	import UserAuthForm from '../../lib/components/UserAuthForm.svelte';
 </script>
 
 <div class="md:hidden">
@@ -8,7 +8,7 @@
 	<img width={1280} height={843} alt="Authentication" class="hidden dark:block" />
 </div>
 <div
-	class="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
+	class="container relative hidden h-[100vh] flex-1 flex-col items-center justify-center bg-background md:grid lg:max-w-none lg:px-0"
 >
 	<Button
 		href="/examples/authentication"
@@ -17,27 +17,7 @@
 	>
 		Login
 	</Button>
-	<div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-		<div
-			class="absolute inset-0 bg-cover"
-			style="
-				background-image:
-					url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80);"
-		/>
-		<div class="relative z-20 flex items-center text-lg font-medium">
-			<!-- <Command class="mr-2 h-6 w-6" /> -->
-			Acme Inc
-		</div>
-		<div class="relative z-20 mt-auto">
-			<blockquote class="space-y-2">
-				<p class="text-lg">
-					&ldquo;This library has saved me countless hours of work and helped me deliver stunning
-					designs to my clients faster than ever before. Highly recommended!&rdquo;
-				</p>
-				<footer class="text-sm">Sofia Davis</footer>
-			</blockquote>
-		</div>
-	</div>
+
 	<div class="lg:p-8">
 		<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
 			<div class="flex flex-col space-y-2 text-center">
