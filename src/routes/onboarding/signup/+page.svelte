20126e566c9d0c673a0206ffc00a45e59f3ba6d4
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-03-28 22:57:54 +0100

    redirect from signup screen

--- src/routes/onboarding/signup/+page.svelte
+++ src/routes/onboarding/signup/+page.svelte
@@ -1,6 +1,32 @@
 <script lang="ts">
 	import Button from '@/components/ui/button/button.svelte';
 	import UserAuthForm from '../../../lib/components/UserAuthForm.svelte';
+	import { goto } from '$app/navigation';
+	import { keycloakState } from '../../../authStore';
+	import api from '@/api';
+
+	let isLoading = false;
+
+	$: if ($keycloakState.authenticated) {
+		redirect().catch(console.error);
+	}
+
+	async function redirect() {
+		// check if the user is correctly set up
+		isLoading = true;
+		const req = await api.callApi('isUserSetupCurrecly', null, 'GET');
+		// const isUserSetupCurrecly = (await req.json()).isUserSetupCurrecly;
+		const isUserSetupCurrecly = true;
+		//TODO: change to actual fetching the data
+
+		if (!isUserSetupCurrecly) {
+			// if not, redirect to the onboarding
+			goto('/onboarding');
+		} else {
+			// else redirect to the workspace
+			goto('/workspace');
+		}
+	}
 </script>
 
 <div class="md:hidden">
@@ -19,7 +45,7 @@
 				<h1 class="text-2xl font-semibold tracking-tight">Create an account</h1>
 				<p class="text-sm text-muted-foreground">Enter your email below to create your account</p>
 			</div>
-			<UserAuthForm actionName="Sign Up" />
+			<UserAuthForm actionName="Sign Up" bind:isLoading />
 			<p class="px-8 text-center text-sm text-muted-foreground">
 				By clicking continue, you agree to our{' '}
 				<a href="/terms" class="underline underline-offset-4 hover:text-primary">
