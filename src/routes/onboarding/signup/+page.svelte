80c87244b750ae2fdeca4ba6bc82931e44eaa920
Jonathan Bangert <jonathan@bangert.dk>
2024-03-29 09:27:28 +0100

    Update links to privacy policy and terms of service

--- src/routes/onboarding/signup/+page.svelte
+++ src/routes/onboarding/signup/+page.svelte
@@ -48,11 +48,11 @@
 			<UserAuthForm actionName="Sign Up" bind:isLoading />
 			<p class="px-8 text-center text-sm text-muted-foreground">
 				By clicking continue, you agree to our{' '}
-				<a href="/terms" class="underline underline-offset-4 hover:text-primary">
+				<a href="https://akademia.cc/terms" class="underline underline-offset-4 hover:text-primary">
 					Terms of Service
 				</a>{' '}
 				and{' '}
-				<a href="/privacy" class="underline underline-offset-4 hover:text-primary">
+				<a href="https://akademia.cc/privacy-policy" class="underline underline-offset-4 hover:text-primary">
 					Privacy Policy
 				</a>
 				.
