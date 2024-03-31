8fd6cc6bb3e50e43fc9e65f48672da7ed681c9e4
Jonathan Bangert <jonathan@bangert.dk>
2024-03-31 23:36:06 +0200

    Show assignment in sidebar

--- src/routes/workspace/sidebar/SidebarAssignment.svelte
+++ src/routes/workspace/sidebar/SidebarAssignment.svelte
@@ -8,11 +8,17 @@
 	import getExtensions from '../editor/tiptap/getExtensions';
 	import api from '@/api';
 	import { Editor, EditorContent } from 'svelte-tiptap';
-	import { YXmlEvent } from 'yjs';
+	import { TiptapTransformer } from '@hocuspocus/transformer';
+	import { onMount } from 'svelte';
+	import * as Y from "yjs";
+	import { AssignmentAnswer, currentFile } from '@/api/apiStore';
 
-	export let sidebarVisible: boolean = true;
+	export let assignmentId: string = $currentFile instanceof AssignmentAnswer ? $currentFile.assignment_id : null;
 
-	export let assignmentId: string = '410f8d03-e429-4809-ab10-0829ce961796';
+	currentFile.subscribe((value) => {
+		if (!(value instanceof AssignmentAnswer)) return;
+		assignmentId = value.assignment_id;
+	});
 
 	async function getDescription() {
 		const res = await api.callApi(`/assignments/${assignmentId}`, null, 'GET');
@@ -22,10 +28,26 @@
 	}
 
 	export let isAssignmentDescriptionOpen: boolean;
-	let dfgdfg = new Editor({
-		extensions: getExtensions(null, false),
+	let editor: Editor;
 
-		editable: false
+	onMount(async () => {
+		// Wait for the assignmentId to be set
+		while (!assignmentId) {
+			await new Promise((resolve) => setTimeout(resolve, 100));
+		}
+
+		const data = await getDescription()
+		console.log("aaaa", typeof data, data, new Uint8Array(data.data));
+		const ydoc = new Y.Doc();
+		Y.applyUpdate(ydoc, new Uint8Array(data.data));
+		const doc = TiptapTransformer.extensions(getExtensions(null, true)).fromYdoc(ydoc);
+		console.log("NEEJ!", doc);
+		// doc.default.content[2] = null;
+		editor = new Editor({
+			extensions: getExtensions(null, true),
+			content: doc.default,
+			editable: false,
+		});
 	});
 </script>
 
@@ -41,7 +63,11 @@
 		Tilbage
 	</button>
 	<div class="assignment-tiptap">
-		<EditorContent editor={dfgdfg} />
+		{#if editor}
+			<EditorContent bind:editor />
+		{:else}
+			<p>Loading...</p>
+		{/if}
 	</div>
 </div>
 
