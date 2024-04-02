0df1541da1bf44defbc3b973610ba12ab9c24f3c
Jonathan Bangert <jonathan@bangert.dk>
2024-04-02 08:22:55 +0200

    only choose class when student

--- src/routes/onboarding/+layout.svelte
+++ src/routes/onboarding/+layout.svelte
@@ -33,7 +33,7 @@
 			'4',
 			'5',
 			$userType !== 'TESTER' ? 'vaelg-skole' : null,
-			$userType !== 'TESTER' ? 'vaelg-klasse' : null,
+			$userType === 'STUDENT' ? 'vaelg-klasse' : null,
 			'done'
 		].filter((it) => it !== null);
 	}
