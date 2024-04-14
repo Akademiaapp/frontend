118e446ccda2d981c0baf5bba84fc0f9811cd197
Jonathan Bangert <jonathan@bangert.dk>
2024-04-14 23:52:10 +0200

    Fix link idfk

--- src/routes/onboarding/done/+page.svelte
+++ src/routes/onboarding/done/+page.svelte
@@ -10,12 +10,12 @@
 
 	onMount(async () => {
 		if ($userType === 'TESTER') {
-			$selectedSchoolId = 'https://forms.gle/S55B7xnTSMoBaXWR6';
+			$selectedSchoolId = '0a6d3842-0ac6-489d-b7ce-3dc298ff30c4';
 		}
 		await api.callApi('/users/self', { type: $userType || 'TESTER' }, 'PUT');
 		await api.callApi(
 			'/users/self',
-			{ schoolId: $selectedSchoolId || 'https://forms.gle/S55B7xnTSMoBaXWR6' },
+			{ schoolId: $selectedSchoolId || '0a6d3842-0ac6-489d-b7ce-3dc298ff30c4' },
 			'PUT'
 		);
 		if ($selectedClassId !== '') {
