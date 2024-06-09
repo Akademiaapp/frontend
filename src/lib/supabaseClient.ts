bd833865490051ee44d1adbeb44ffcb0800591c0
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-06-09 15:36:05 +0200

    formatting

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
-import { Database } from './supabase.types'
+import { createClient } from '@supabase/supabase-js';
+import { Database } from './supabase.types';
 
-export const supabase = createClient<Database>('https://khpnlpgmzmocwqjkvemv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtocG5scGdtem1vY3dxamt2ZW12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4NDIwNTIsImV4cCI6MjAzMzQxODA1Mn0.PfFI_eQ5qU6t9mlFUePdnVJHX6Xqt0UDAdUsTXWCPDI')
+export const supabase = createClient<Database>(
+	'https://khpnlpgmzmocwqjkvemv.supabase.co',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtocG5scGdtem1vY3dxamt2ZW12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4NDIwNTIsImV4cCI6MjAzMzQxODA1Mn0.PfFI_eQ5qU6t9mlFUePdnVJHX6Xqt0UDAdUsTXWCPDI'
+);
