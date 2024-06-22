543344795defda5db5cc1e7108c1a5cd96cc34ce
Jonathan Bangert <jonathan@bangert.dk>
2024-06-22 15:13:34 +0200

    .

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({ lastInfo: standardGetLast("https://app.akademia.cc/versionedWorker.json", isTestBuild)}),
+		adapter: adapter({ lastInfo: standardGetLast("https://app.akademia.cc/versionedWorker.json", true)}),
 		alias: {
 			'@/*': './src/lib/*'
 		}
