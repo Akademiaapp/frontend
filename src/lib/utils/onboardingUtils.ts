4e0548ced9f6b9b5f536c0c0a9990631386313d3
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-06-26 18:53:32 +0200

    maybe fix

--- src/lib/utils/onboardingUtils.ts
+++ src/lib/utils/onboardingUtils.ts
@@ -7,7 +7,7 @@ import { page } from '$app/stores';
 
 export async function redirect() {
 	// check if the user is correctly set up
-	console.log('AIOSHDIUHSIDUHUIAHSIDHOIAHSDUI')
+	console.log('AIOSHDIUHSIDUHUIAHSIDHOIAHSDUI');
 	const isUserSetupCurrecly = await isUserSetupCurrectly();
 
 	if (!isUserSetupCurrecly && get(isOnline)) {
@@ -15,19 +15,20 @@ export async function redirect() {
 		goto('/onboarding');
 	} else {
 		if (!get(page).url.pathname.includes('onboarding')) return;
-		console.log("Redirecting!");
+		console.log('Redirecting!');
 		// else redirect to the workspace
 		goto('/workspace/home');
 	}
 }
 
 export async function isUserSetupCurrectly() {
+	if (!get(session).user) return false;
 	const { data, error } = await supabase
 		.from('user')
 		.select('*')
 		.eq('id', get(session).user.id)
 		.single();
-	
+
 	if (error || !data.type || !data.school_id) {
 		return false;
 	} else {
