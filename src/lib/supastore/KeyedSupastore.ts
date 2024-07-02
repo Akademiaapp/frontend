bb5142bfbb0b298bf19facca420ea79932b557bd
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-07-03 01:18:33 +0200

    Update KeyedSupastore.ts

--- src/lib/supastore/KeyedSupastore.ts
+++ src/lib/supastore/KeyedSupastore.ts
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 import { SupaStore } from './SupaStore';
-import type { GenericDatabase, TableInsert, TableRow } from './types';
+import type { ClientRow, GenericDatabase, TableInsert, TableRow } from './types';
 import type { Compare } from './compare';
 
 export class KeyedSupaStore<
@@ -10,7 +10,7 @@ export class KeyedSupaStore<
 	TRow extends TableRow<D, T> = TableRow<D, T>,
 	TInsert extends TableInsert<D, T> = TableInsert<D, T>
 > extends SupaStore<D, T> {
-	keyedStore = writable<Partial<Record<string | number, (TableRow<D, T> & { cid: number })[]>>>({});
+	keyedStore = writable<Record<string | number, ClientRow<D, T>[]>>({});
 	key: keyof TableRow<D, T> = 'path';
 
 	setKey(key: keyof TableRow<D, T>) {
@@ -22,27 +22,14 @@ export class KeyedSupaStore<
 		this.store.subscribe((data) => {
 			this.keyedStore.set(Object.groupBy(data, (d) => d[this.key] as string));
 		});
-	}
 
-	override insert(
-		d: TInsert,
-		server?: boolean
-	): Promise<Partial<TableRow<D, T>> & TableRow<D, T> & { cid: number }> {
-		this.keyedStore.update((prev) => {
-			const cid = this.getData().length + 1;
-			const clientRow = {
-				...this.deafults(),
-				...(d as undefined as TRow),
-				cid
-			};
-			prev[clientRow[this.key] as string] = [
-				...(prev[clientRow[this.key] as string] ?? []),
-				clientRow
-			];
-
-			return prev;
+		this.eventHandler.on('insert', (clientRow: ClientRow<D, T>) => {
+			this.keyedStore.update((prev) => {
+				const key = clientRow[this.key] as string;
+				prev[key] = [...(prev[key] ?? []), clientRow];
+				return prev;
+			});
 		});
-		return super.insert(d, server);
 	}
 
 	async _delete(compare: Compare, server?: boolean) {
@@ -64,13 +51,13 @@ export class KeyedSupaStore<
 		this.keyedStore.set(Object.groupBy(this.getData(), (d) => d[this.key] as string));
 	}
 
-	override async _update(changes, compare: Compare, server?: boolean): Promise<void> {
+	override async _update(changes, compare: Compare, server?: boolean) {
 		const r = await super._update(changes, compare, server);
 		this._group();
 		return r;
 	}
 
-	override async forceFetch(update = true): Promise<TRow[]> {
+	override async forceFetch(update = true) {
 		const r = await super.forceFetch(update);
 		this._group();
 		return r as TRow[];
